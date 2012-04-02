d33297e27385efa6dca5c0a31b9487bfe092af08
Tianwei Liu <tianwei@wimm.com>
2012-04-02 11:40:20 -0700

    changed tab to spaces and removed trailing whitespace

--- src/com/wimm/demo/magnetometerdemo/CompassView.java
+++ src/com/wimm/demo/magnetometerdemo/CompassView.java
@@ -11,30 +11,28 @@ import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.view.View;
 
-// A simple compass view consisting a circle and a line.
+// A simple compass view consisting of a circle and a line.
 public class CompassView extends View {
 
-	public CompassView(Context context, AttributeSet attrs) {
-		super(context, attrs);
-		mPaint = new Paint();
-		mPaint.setAntiAlias(true);
+    public CompassView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        mPaint = new Paint();
+        mPaint.setAntiAlias(true);
         mPaint.setStyle(Paint.Style.STROKE);
-		mPaint.setStrokeWidth(2);
-		mPaint.setColor(Color.RED);
+        mPaint.setStrokeWidth(2);
+        mPaint.setColor(Color.RED);
+    }
 
-	}
-	
-	private Paint mPaint;
-	private float mRotationDegrees = 0;
-	
+    private Paint mPaint;
+    private float mRotationDegrees = 0;
 
-	@Override
-	protected void onDraw(Canvas canvas) {
+    @Override
+    protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
-		float xCenter = (float) (getWidth() / 2.0);
-		float yCenter = (float) (getHeight() / 2.0);
-		
+        float xCenter = (float) (getWidth() / 2.0);
+        float yCenter = (float) (getHeight() / 2.0);
+
         float radius = Math.max(xCenter, yCenter);
         canvas.drawCircle(xCenter, yCenter, radius, mPaint);
 
@@ -43,11 +41,11 @@ public class CompassView extends View {
         canvas.rotate(mRotationDegrees,xCenter, yCenter);
         canvas.drawLine(xCenter, yCenter, xCenter, yCenter-radius, mPaint);
         canvas.restore();
-	}
+    }
 
-	// Update our compass rotation and request a redraw.
-	public void setRotation(float degrees) {
-		mRotationDegrees = degrees;
-		invalidate();
-	}
+    // Update our compass rotation and request a redraw.
+    public void setRotation(float degrees) {
+        mRotationDegrees = degrees;
+        invalidate();
+    }
 }
